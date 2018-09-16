9bea9073d9b82973a49fb1f90deffb05e93c7718
Bennett Gale <bennett.p.gale@gmail.com>
2018-09-16 20:04:32 +1000

    removed indirection

--- gss.c
+++ gss.c
@@ -1,28 +1,28 @@
 #include <math.h>
 
-void gss(double *xl, double *xu, double (*func)(double), double *xopt)
+double gss(double xl, double xu, double (*func)(double))
 {
 	double R = 0.61803;
-	double d = R * (*xu - *xl);
-	double x1 = *xl + d;
-	double x2 = *xu - d;
-	double fx1 = 0, fx2 = 1;
+	double d = R * (xu - xl);
+	double x1 = xl + d;
+	double x2 = xu - d;
+	double fx1 = func(x1), fx2 = func(x2);
 
 	while (fabs(fx1 - fx2) > 3e-8) {
 		fx1 = func(x1);
 		fx2 = func(x2);
 		if (fx1 < fx2) {
-			*xl = x2;
+			xl = x2;
 			x2 = x1;
-			d = R * (*xu - *xl);
-			x1 = *xl + d;
+			d = R * (xu - xl);
+			x1 = xl + d;
 		} else if (fx2 < fx1) {
-			*xu = x1;
+			xu = x1;
 			x1 = x2;
-			d = R * (*xu - *xl);
-			x2 = *xu - d;
+			d = R * (xu - xl);
+			x2 = xu - d;
 		} 
 	}
-	*xopt = (x1 + x2) / 2;
+	return (x1 + x2) / 2;
 }
 
--- gss.h
+++ gss.h
@@ -4,9 +4,9 @@
 /**
  * xl and xu are initial estimates and must bracket exactly one minimum function value. 
  * func is the objective function to be minimised.
- * xopt is assigned the x value which minimises the objective function
+ * Returns the x value which minimises the objective function
  **/
-void gss(double *xl,  double xu, double (*func)(double), double *xopt);
+void gss(double xl,  double xu, double (*func)(double));
 
 #endif
 
